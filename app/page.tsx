e461e9680a0a804a4a8a886a29a8edd548447b1d
SofiaCoder <sofia.laas@gmail.com>
2025-11-20 11:12:59 +0100

    Mobile bg added

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Countdown from './component/Countdown';
 
 export default function Home() {
   return (
-    <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black bg-[url("/img/Kambodja.png")] bg-cover bg-center'>
+    <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black bg-[url("/img/MobileKambodja.png")] sm:bg-[url("/img/Kambodja.png")] bg-cover bg-center'>
       <Countdown target='2026-02-05T00:00:00' />
     </div>
   );
